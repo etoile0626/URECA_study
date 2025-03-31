f7bfd056bc5ffedec0aad962b1ef37b36c7ed676
yoon_je <67690073+etoile0626@users.noreply.github.com>
2025-03-31 15:25:41 +0900

    Add files via upload

--- spring/jslt_el_jquery/main/java/kr/co/ureca/jquery/controller/JQController.java
+++ spring/jslt_el_jquery/main/java/kr/co/ureca/jquery/controller/JQController.java
@@ -6,6 +6,61 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class JQController {
 	
+	@GetMapping("/jq17")
+	public String jq17Traverse() {
+		return "jq17traverse";
+	}
+	
+	@GetMapping("/jq16")
+	public String jq16Parents() {
+		return "jq16parents";
+	}
+	
+	@GetMapping("/jq15")
+	public String jq15AddClass() {
+		return "jq15addclass";
+	}
+	
+	@GetMapping("/jq14")
+	public String jq14Search() {
+		return "jq14search";
+	}
+	
+	@GetMapping("/jq13")
+	public String jq13NthChild() {
+		return "jq13nthchild";
+	}
+	
+	@GetMapping("/jq12")
+	public String jq12TableHover() {
+		return "jq12tablehover";
+	}
+	
+	@GetMapping("/jq11")
+	public String jq11SlideUpDown() {
+		return "jq11slideupdown";
+	}
+	
+	@GetMapping("/jq10")
+	public String jq10FadeInOut() {
+		return "jq10fadeinout";
+	}
+	
+	@GetMapping("/jq9")
+	public String jq9HideShow() {
+		return "jq9hideshow";
+	}
+	
+	@GetMapping("/jq8")
+	public String jq8Selector() {
+		return "jq8selector";
+	}
+	
+	@GetMapping("/jq7")
+	public String jq7Selector() {
+		return "jq7selector";
+	}
+	
 	@GetMapping("/jq6")
 	public String jq6Selector() {
 		return "jq6selector";
